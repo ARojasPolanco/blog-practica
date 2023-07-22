57bc262e676de5ec00a297a68c3783687a4c3ea4
ARojasPolanco <advbrrop23@gmail.com>
2023-07-22 19:26:49 -0300

    Beta 0.1

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('btn')
+const menu = document.getElementById('nav-menu')
+
+button.addEventListener('click', function(){
+    menu.classList.toggle('show')
+})
+
